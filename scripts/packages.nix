adeb418cd83c4b5175e8fb5c92e514d5f05c0ad5
LightDiscord <root@arnaud.sh>
2018-10-28 19:18:04 +0100

    Evaluate packages for a given channel.

---
+++ scripts/packages.nix
@@ -0,0 +1,59 @@
+{ pkgs ? import <nixpkgs> { }, channel }:
+
+with pkgs.lib;
+with builtins;
+
+let
+
+  src = fetchTarball "https://github.com/nixos/nixpkgs-channels/archive/${channel}.tar.gz";
+  override = fetchurl https://raw.githubusercontent.com/NixOS/nixos-homepage/master/discover-more-packages-config.nix;
+  packages = import src { config = import override; };
+
+  details = package: let
+
+    name = (tryEval package.name or "").value;
+    description = (tryEval package.meta.description or "").value;
+
+  in if (isBool name && !name) then null else {
+    _type = "package";
+
+    inherit name description;
+  };
+
+  recursing = value: isAttrs value
+    && !(isDerivation value)
+    && value ? recurseForDerivations
+    && value.recurseForDerivations;
+
+  transform = let
+
+    process = value: if (recursing value) then (transform value)
+      else if (isDerivation value) then (details value)
+      else null;
+
+  in mapAttrs (_: x: process (tryEval x).value);
+
+  removeEmpty = filterAttrsRecursive (_: x: x != null);
+
+  isPackage = value: value ? "_type"
+    && value._type == "package";
+
+  definePackages = mapAttrsRecursiveCond
+    (value: !(isPackage value))
+    (path: nameValuePair (concatStringsSep "." path));
+
+  collectPackages = collect (x: x ? value && x ? name);
+
+  flatten = packages: mapAttrs
+    (_: filterAttrs (n: _: n != "_type"))
+    (listToAttrs packages);
+
+  chain = flatten (collectPackages (definePackages (removeEmpty (transform packages))));
+
+  text = toJSON chain;
+
+in pkgs.writeTextFile {
+  name = "packages-${channel}";
+
+  inherit text;
+}
